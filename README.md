0fb9e3484c902e029ea80c5400b0dd61a9028152
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2021-03-20 03:38:58 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [additions](#additions)
+
+
+## additions
+1- I have added all the necessary js function to get elements from DOM and manipulate it to get desired functionality 
+2- Also added the css style of scroll-behavior to be smooth
+3- I have class="section" to all sections so i can get all of them with querySelectorAll()
+4- Attached script link at bottom of the body of html file
---
+++ js/app.js
@@ -0,0 +1,85 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const menu =document.getElementById('navbar__list');
+const sections =Array.from(document.querySelectorAll('section'));
+const sectionCount =sections.length;
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+function createListItem (){
+    for(section of sections){
+                   /*getting section name and id */ 
+        const sectionName= section.getAttribute('data-nav');
+        const sectionLink= section.getAttribute('id');
+                   /* creating a list item and and with section name and id */
+        const listItem =document.createElement('li');
+        listItem.innerHTML=`<a href="#${sectionLink}" class="menu__link">${sectionName}</a>`;
+                    /* append the list item to the navbar*/
+        menu.appendChild(listItem);
+
+    }
+}
+
+function inViewPort(elem){
+    return(elem.getBoundingClientRect()>=0);
+}
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+function activateSection(){
+       for(section of sections){
+           if(inViewPort(section)){
+               // if it's not already activated  
+               if(!section.classList.contains('your-active-class'))
+                //then activate it
+               section.classList.add('your-active-class');
+           }else{
+               //if not in view port make sure it's not active
+            section.classList.remove('your-active-class');
+           }
+       }
+}
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+createListItem();
+// Scroll to section on link click
+
+// Set sections as active
+document.addEventListener('scroll',activateSection());
+
